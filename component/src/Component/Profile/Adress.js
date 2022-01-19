d37a04f368160d611ada1477df29f1efb7a3e455
AminaGMCStudent <amina.hedhili@gmail.com>
2022-01-19 08:29:02 +0100

    first commit

---
+++ component/src/Component/Profile/Adress.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Adress = () => {
+    return (
+        <div>
+                    <h2>Hammam Sousse City</h2>
+    
+        </div>
+    )
+}
+
+export default Adress
